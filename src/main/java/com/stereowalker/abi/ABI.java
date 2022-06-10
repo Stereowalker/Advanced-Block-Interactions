f06a8e78c7a599e2b61c7359442126ad3ad601db
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-06-10 14:46:22 +0100

    Initial fabric port

--- src/main/java/com/stereowalker/abi/ABI.java
+++ src/main/java/com/stereowalker/abi/ABI.java
@@ -6,10 +6,7 @@ import org.apache.logging.log4j.Logger;
 import com.stereowalker.unionlib.mod.MinecraftMod;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod(value = ABI.MOD_ID)
 public class ABI extends MinecraftMod
 {
 	public static ABI instance;
@@ -19,7 +16,6 @@ public class ABI extends MinecraftMod
 	public ABI() 
 	{
 		super(MOD_ID, new ResourceLocation(MOD_ID, "textures/mod.png"), LoadType.BOTH);
-		MinecraftForge.EVENT_BUS.register(this);
 		instance = this;
 	}
 
